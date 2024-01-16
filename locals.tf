0d3961c7f3fe3d62f776d1691c4074b9276e7097
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-15 19:57:50 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,33 @@
+#locals
+
+locals {
+ 
+  service_name = "forum"
+  owner = "Community Team"
+  lengthofservice_name=length(local.service_name)
+favoritclublist=["RealMadrid","Alhilal","ACMILAN"]
+
+}
+locals{
+  servicename="abracadabra"
+  forum="abracadabramcit"
+  lengthsa=length(local.servicename)
+  lengthforum=length(local.forum)
+  winterlistOfSports=["icehockey","snowboarding","iceskating"]
+  total_output = ["150", "150", "150"]
+}
+
+
+locals {
+ local_flattened_list = flatten(var.nested_list)
+}
+
+locals {
+ flattened_map = flatten([for k, v in var.nested_map : [for kk, vv in v: {group=k, key = kk, value = vv }]])
+}
+
+
+
+locals {
+ my_value = lookup(var.my_map, "key1", "")
+}
