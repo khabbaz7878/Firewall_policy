f8804c2a3a40c6f9f8541c64f712031354f7d880
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-15 20:01:16 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -13,3 +13,36 @@ resource "azurerm_virtual_network" "example" {
 
  
 }
+resource "azurerm_resource_group" "mcit420zz5um" {
+  name     = "database-rg"
+  location = "West Europe"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "mssqlserver"
+  resource_group_name          = azurerm_resource_group.mcit420zz5um.name
+  location                     = azurerm_resource_group.mcit420zz5um.location
+  version                      = "12.0"
+  administrator_login          = var.db_login
+  administrator_login_password = var.db_password
+  minimum_tls_version          = "1.2"
+}
+resource "azurerm_resource_group" "example" {
+ name     = "example-resources"
+ location = "Canada east"
+}
+
+resource "azurerm_virtual_network" "example" {
+ name                = "example-vnet"
+ address_space       = ["10.0.0.0/16"]
+ location            = azurerm_resource_group.example.location
+ resource_group_name = azurerm_resource_group.example.name
+
+ tags = {
+    environment = "example"
+ }
+}
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "my-resource-group"
+  location = "eastus"
+}
