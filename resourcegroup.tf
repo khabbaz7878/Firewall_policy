5fb4a5d573b4d107d08d87080b7f0ae3a97209c7
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-15 19:15:29 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "example" {
  address_space       = ["10.0.0.0/16"]
  location            = azurerm_resource_group.example.location
  resource_group_name = azurerm_resource_group.example.name
-
+}
  tags = {
     environment = "example"
  }
