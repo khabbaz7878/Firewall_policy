78882a84168e6f7652e00c205c88c138e5886fc9
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-15 19:19:58 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -5,3 +5,13 @@ resource "azurerm_virtual_network" "waf" {
  resource_group_name = azurerm_resource_group.example.name
 }
  
+resource "azurerm_virtual_network" "example" {
+ name                = "example-virtual-network"
+ location            = azurerm_resource_group.example.location
+ resource_group_name = azurerm_resource_group.example.name
+ address_space       = ["10.0.0.0/16"]
+
+ features {
+    ip_flow_verify = true
+ }
+}
