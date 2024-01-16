ddde67578773cd94f5abcfe896b7f10741a2f229
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-15 19:17:03 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "waf" {
  name                = "example-vnet"
  address_space       = ["10.0.0.0/16"]
  location            = azurerm_resource_group.example.location
